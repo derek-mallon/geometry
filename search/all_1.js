ecedb0604f1fdd85f8e7e3a15824b4ae0f613b82
Derek Mallon <derekmallon@tutanota.com>
2016-06-17 00:39:15 -0400

    fixed the docs

---
+++ search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['xy',['XY',['../struct_x_y.html',1,'XY'],['../struct_x_y.html#af892d7ce4ec8ef817e791ab783159890',1,'XY::XY()']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "ax",
+  1: "ax",
+  2: "x"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions"
+};
+
